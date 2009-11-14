55f5aa48e7921f5ab93273abb65752f88316c5f9
Eric Park <epark@yowie.local>
2009-11-14 02:18:52 -0800

    fixed tabbing

--- app/views.py
+++ app/views.py
@@ -17,74 +17,72 @@ def home(request):
 
 
 def user_login(request):
-    # TODO: GET -> POST
-    if request.method == 'POST':
-        try:
-            username = request.REQUEST['u']
-            password = request.REQUEST['p']
-        except KeyError:
-            return render_to_response('user_login.html', {
-                    'message' : 'invalid username or password'
-                    })
+	# TODO: GET -> POST
+	if request.method == 'POST':
+		try:
+			username = request.REQUEST['username']
+			password = request.REQUEST['password']
+		except KeyError:
+			return render_to_response('user_login.html', {
+				'message' : 'invalid username or password'
+			})
 
-        user = auth.authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                auth.login(request, user)
-                return render_to_response('home.html', {
-                        'message' : 'login successful',
-                        })
+		user = auth.authenticate(username=username, password=password)
+		if user is not None:
+			if user.is_active:
+				auth.login(request, user)
+				return render_to_response('home.html', {
+					'message' : 'login successful',
+				})
 
-            return render_to_response('user_login.html', {
-                    'message' : 'invalid user',
-                    })
-    else:
-        return render_to_response('user_login.html', {
-                'message' : 'login required',
-                })
-
-	
-def user_register(request):
-    if request.method == 'POST':
-        username		= request.REQUEST['username']
-        email			= request.REQUEST['email']
-        password		= request.REQUEST['password']
-        repassword              = request.REQUEST['repassword']
-        first_name		= request.REQUEST['first_name']
-        last_name		= request.REQUEST['last_name']
-
-        templ = {
-            'username': username,
-            'first_name': first_name,
-            'last_name': last_name,
-            'password': password,
-            'repassword': repassword,
-            'email': email
-            }
-
-        if password != repassword:
-            templ['message'] = 'Error: passwords must match'
-            return render_to_response('user_register.html', templ)
-                                             
-        try:
-            user = User.objects.create_user(
-                username=username, 
-                email=email, 
-                password=password)
-        except IntegrityError:
-            templ['message'] = 'Error: username already taken'
-            return render_to_response('user_register.html', templ)
-
-        user.first_name = first_name
-        user.last_name = last_name
-        user.save()
-
-        return render_to_response('home.html', {
-                'message' : 'registration successful',
+		return render_to_response('user_login.html', {
+			'message' : 'invalid user',
 		})
-	
-    else:
-        return render_to_response('user_register.html', {})
+	else:
+		return render_to_response('user_login.html', {})
+
+
+def user_register(request):
+	if request.method == 'POST':
+		username		= request.REQUEST['username']
+		email			= request.REQUEST['email']
+		password		= request.REQUEST['password']
+		repassword  	= request.REQUEST['repassword']
+		first_name		= request.REQUEST['first_name']
+		last_name		= request.REQUEST['last_name']
+
+		templ = {
+			'username': username,
+			'first_name': first_name,
+			'last_name': last_name,
+			'password': password,
+			'repassword': repassword,
+			'email': email
+		}
+
+		if password != repassword:
+			templ['message'] = 'Error: passwords must match'
+			return render_to_response('user_register.html', templ)
+
+		try:
+			user = User.objects.create_user(
+				username=username, 
+				email=email, 
+				password=password)
+		except IntegrityError:
+			templ['message'] = 'Error: username already taken'
+			return render_to_response('user_register.html', templ)
+
+		user.first_name = first_name
+		user.last_name = last_name
+		user.save()
+
+		return render_to_response('home.html', {
+			'message' : 'registration successful',
+		})
+
+	else:
+		return render_to_response('user_register.html', {})
 
 
 @login_required
@@ -102,10 +100,10 @@ def user_register_twitter(request):
             user	= request.user,
             username	= username,
             password	= password,
-	)
+		)
         return render_to_response('home.html', {
                 'message' : 'twitter integration successful',
-	       })
+		})
 
     else:
         return render_to_response('user_register_twitter.html', {})
